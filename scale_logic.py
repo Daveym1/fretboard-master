14d6ef2265714e5db2920e2657b222eadebfed98
Dave <davemarsh1@gmail.com>
2026-01-04 21:30:40 +0000

    added note names and scale interval lists

---
+++ scale_logic.py
@@ -0,0 +1,13 @@
+notes_sharp = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
+notes_flat = ["A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab"]
+scale_intervals = {
+    "major": [2, 2, 1, 2, 2, 2, 1],
+    "minor": [2, 1, 2, 2, 1, 2, 2],
+    "dorian": [2, 1, 2, 2, 2, 1, 2],
+    "phrygian": [1, 2, 2, 2, 1, 2, 2],
+    "lydian": [2, 2, 2, 1, 2, 2, 1],
+    "mixolydian": [2, 2, 1, 2, 2, 1, 2],
+    "locrian": [1, 2, 2, 1, 2, 2, 2],
+    "harmonic_minor": [2, 1, 2, 2, 1, 3, 1],
+    "melodic_minor": [2, 1, 2, 2, 2, 2, 1],
+}     
